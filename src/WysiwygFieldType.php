50cc0e83ae94e971987cb57aab5291af31cb56b8
Ryan Thompson <service@aiwebsystems.com>
2014-08-22 20:09:22 -0500

    Initial commit.

---
+++ src/WysiwygFieldType.php
@@ -0,0 +1,30 @@
+<?php namespace Addon\FieldType\Wysiwyg;
+
+use Streams\Core\Addon\FieldTypeAbstract;
+
+class WysiwygFieldType extends FieldTypeAbstract
+{
+    /**
+     * The database column type this field type uses.
+     *
+     * @var string
+     */
+    public $columnType = 'text';
+
+    /**
+     * Field type version
+     *
+     * @var string
+     */
+    public $version = '1.1.0';
+
+    /**
+     * Field type author information.
+     *
+     * @var array
+     */
+    public $author = array(
+        'name' => 'AI Web Systems, Inc.',
+        'url'  => 'http://aiwebsystems.com/',
+    );
+}
