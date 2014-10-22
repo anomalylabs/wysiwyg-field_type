d669e9041c23cd993c6dab07f06fd2c4b4ae1fad
Ryan Thompson <service@aiwebsystems.com>
2014-10-22 13:50:27 -0500

    Clean up before final

--- src/WysiwygFieldType.php
+++ src/WysiwygFieldType.php
@@ -4,29 +4,5 @@ use Anomaly\Streams\Platform\Addon\FieldType\FieldTypeAddon;
 
 class WysiwygFieldType extends FieldTypeAddon
 {
-    protected $slug = 'wysiwyg';
-
-    /**
-     * The database column type this field type uses.
-     *
-     * @var string
-     */
     public $columnType = 'text';
-
-    /**
-     * Field type version
-     *
-     * @var string
-     */
-    public $version = '1.1.0';
-
-    /**
-     * Field type author information.
-     *
-     * @var array
-     */
-    public $author = array(
-        'name' => 'AI Web Systems, Inc.',
-        'url'  => 'http://aiwebsystems.com/',
-    );
 }
