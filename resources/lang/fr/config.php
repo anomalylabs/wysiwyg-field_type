7ca88136562411738f267160a0577c94ccafc292
MarceauKa <marceau@casals.fr>
2015-07-15 18:18:38 +0200

    Ajout de la traduction française

---
+++ resources/lang/fr/config.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'buttons'     => [
+        'label'        => 'Boutons',
+        'instructions' => 'Choisissez les boutons à afficher.'
+    ],
+    'plugins'     => [
+        'label'        => 'Plugins',
+        'instructions' => 'Choisissez les plugins de l\'éditeur à activer.'
+    ],
+    'height'      => [
+        'label'        => 'Hauteur',
+        'instructions' => 'Choisissez la hauteur de l\'éditeur.'
+    ],
+    'line_breaks' => [
+        'label'        => 'Séparateur de ligne',
+        'instructions' => 'Souhaitez-vous utiliser des séparateurs de ligne plutôt que des paragraphes ?'
+    ]
+];
---
+++ resources/lang/fr/plugin.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'table'        => 'Table',
+    'video'        => 'Vidéo',
+    'fontsize'     => 'Taille',
+    'fullscreen'   => 'Plein écran',
+    'filemanager'  => 'Gestion de fichier',
+    'imagemanager' => 'Gestion d\'image'
+];
