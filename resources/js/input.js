9f3991810e81327d5d4fa6d4d1ec338b71b42cbb
Ryan Thompson <service@aiwebsystems.com>
2016-03-18 01:15:36 -0500

    Strip script tags

--- resources/js/input.js
+++ resources/js/input.js
@@ -50,6 +50,7 @@ $(function () {
             /**
              * Settings
              */
+            script: false,
             cleanOnPaste: true,
             replaceTags: [
                 ['strike', 'del'],
