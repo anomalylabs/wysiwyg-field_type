28a050b3a6b7cccdc9c60af573855f61c991be0e
Ryan Thompson <ryan@pyrocms.com>
2016-07-12 20:22:17 -0500

    Update to redactor 1.2.5 and add link tooltip

--- resources/js/input.js
+++ resources/js/input.js
@@ -51,6 +51,7 @@ $(function () {
              * Settings
              */
             script: false,
+            linkTooltip: true,
             cleanOnPaste: true,
             removeEmpty: ['strong', 'em', 'p'],
 
