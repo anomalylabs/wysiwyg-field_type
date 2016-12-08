08d25f72e3325ed37264524d6493fba2e5f3d55f
samharrison7 <samharrison.xg@gmail.com>
2016-12-08 11:26:44 +0000

    Making work in subfolder installations
    Added REQUEST_ROOT_PATH to request paths to make sure it doesn't 404 in subfolder installations of Pyro.

--- resources/js/plugins/imagemanager.js
+++ resources/js/plugins/imagemanager.js
@@ -35,7 +35,7 @@
                 $('#' + this.opts.element.attr('name') + '-modal')
                     .modal('show')
                     .find('.modal-content')
-                    .load('/streams/wysiwyg-field_type/index?' + params);
+                    .load(REQUEST_ROOT_PATH + '/streams/wysiwyg-field_type/index?' + params);
             },
             upload: function() {
 
@@ -46,7 +46,7 @@
                 $('#' + this.opts.element.attr('name') + '-modal')
                     .modal('show')
                     .find('.modal-content')
-                    .load('/streams/wysiwyg-field_type/choose?' + params);
+                    .load(REQUEST_ROOT_PATH + '/streams/wysiwyg-field_type/choose?' + params);
             },
             insert: function(e) {
 
@@ -66,7 +66,7 @@
                     return false;
                 }
 
-                var url = '/files/' + file;
+                var url = REQUEST_ROOT_PATH + '/files/' + file;
 
                 this.insert.node($('<img />').attr('src', url));
 
