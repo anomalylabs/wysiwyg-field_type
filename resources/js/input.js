c57ff3fed30858a81df70fe8494c77684e3ad9c1
Denis Efremov <efremov.a.denis@gmail.com>
2017-04-29 07:35:32 +0300

    Update input.js

--- resources/js/input.js
+++ resources/js/input.js
@@ -6,14 +6,15 @@ $(document).on('ajaxComplete ready', function () {
         /**
          * Gather available buttons / plugins.
          */
-        var buttons = $(this).data('available_buttons');
-        var plugins = $(this).data('available_plugins');
+        var $this = $(this);
+        var buttons = $this.data('available_buttons');
+        var plugins = $this.data('available_plugins');
 
-        $(this).addClass('hasEditor');
+        $this.addClass('hasEditor');
 
-        $(this).redactor({
+        $this.redactor({
 
-            element: $(this),
+            element: $this,
 
             /**
              * Initialize the editor icons.
@@ -36,7 +37,6 @@ $(document).on('ajaxComplete ready', function () {
                     });
 
                     $.each(this.button.all(), $.proxy(function (i, s) {
-
                         var key = $(s).attr('rel');
 
                         if (typeof icons[key] !== 'undefined') {
@@ -44,11 +44,8 @@ $(document).on('ajaxComplete ready', function () {
                             var button = this.button.get(key);
                             this.button.setIcon(button, icon);
                         }
-
                     }, this));
-
-                    emmetCodeMirror(this.codemirror.editor);
-                }
+                },
             },
 
             /**
@@ -67,44 +64,11 @@ $(document).on('ajaxComplete ready', function () {
             /**
              * Features
              */
-            minHeight: $(this).data('height'),
-            placeholder: $(this).attr('placeholder'),
-            folders: $(this).data('folders').toString().split(','),
-            buttons: $(this).data('buttons').toString().split(','),
-            plugins: $(this).data('plugins').toString().split(','),
-            codemirror: {
-                theme: 'mbo',
-                lineNumbers: true,
-                mode: 'php',
-                indentUnit: 4,
-                profile: 'xhtml',
-                lineWrapping: true,
-                tabSize: 4,
-                indentWithTabs: 'spaces',
-                showCursorWhenSelecting: true,
-                cursorScrollMargin: 2,
-                cursorHeight: 0.95,
-                lineWiseCopyCut: false,
-                viewportMargin: Infinity,
-                // allowDropFileTypes: ['image/jpg', 'image/png', 'image/gif'],
-                autoCloseBrackets: true,
-                highlightSelectionMatches: true,
-                keyMap: 'sublime',
-                // lint: true,
-                // matchBrackets: true,
-                styleActiveLine: false,
-                gutters: ['CodeMirror-lint-markers'],
-                extraKeys: {
-                    F10: function (cm) {
-                        cm.setOption('fullScreen', !cm.getOption('fullScreen'));
-                    },
-                    Esc: function (cm) {
-                        if (cm.getOption('fullScreen')) {
-                            cm.setOption('fullScreen', false);
-                        }
-                    }
-                }
-            }
+            minHeight: $this.data('height'),
+            placeholder: $this.attr('placeholder'),
+            folders: $this.data('folders').toString().split(','),
+            buttons: $this.data('buttons').toString().split(','),
+            plugins: $this.data('plugins').toString().split(',')
         });
     });
 });
